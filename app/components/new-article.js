c6afc662c523ad995a389e0412b18a0d16cd3ea6
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-03-21 13:10:07 -0700

    add 'add new article' functionality

---
+++ app/components/new-article.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewArticle: false,
+  actions: {
+    articleFormShow() {
+      this.set('addNewArticle', true);
+    },
+
+    saveArticle() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        body: this.get('body'),
+        image: this.get('image'),
+      };
+      this.set('addNewArticle', false);
+      this.sendAction('saveArticle', params);
+    }
+  }
+});

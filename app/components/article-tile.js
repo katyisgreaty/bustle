95e4ece6489a9d4be3fa63b4b058fcf74c3f3982
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-03-21 15:56:25 -0700

    added update function to admin page- works

--- app/components/article-tile.js
+++ app/components/article-tile.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isBodyShowing: false,
+  isFeatured: false,
   actions: {
     bodyShow: function() {
       this.set('isBodyShowing', true);
     },
     bodyHide: function() {
       this.set('isBodyShowing', false);
+    },
+    featuredShow: function() {
+      this.set('isFeatured', true);
+    },
+    featuredHide: function() {
+      this.set('isFeatured', false);
     }
   }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -13,10 +13,27 @@ export default Ember.Route.extend({
     },
 
     destroyArticle(article) {
-        if (confirm('Are you sure you want to delete this article?')) {
-      article.destroyRecord();
-      this.transitionTo('admin');
-    }
+      if (confirm('Are you sure you want to delete this article?')) {
+        article.destroyRecord();
+        this.transitionTo('admin');
+      }
+    },
+
+    // markFeatured(article) {
+    //   if (confirm('Are you sure you want to mark this article as featured?')) {
+    //     // this.featuredShow();
+    //     this.transitionTo('index');
+    //   }
+    // }
+
+    update(article, params) {
+      Object.keys(params).forEach(function(key) {
+   if(params[key]!==undefined) {
+     article.set(key,params[key]);
+   }
+ });
+      article.save();
+      this.transitionTo('index');
     }
   }
 });

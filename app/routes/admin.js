9bec9b95e3b986b0639979a1488e98c6ba062a02
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-03-21 13:57:26 -0700

    add delete function to admin site that is not functional

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -10,6 +10,17 @@ export default Ember.Route.extend({
       var newArticle = this.store.createRecord('article', params);
       newArticle.save();
       this.transitionTo('index');
+    },
+
+    delete(article) {
+      if (confirm('Are you sure you want to delete this article?')) {
+        this.sendAction('destroyArticle', article);
+      }
+    },
+
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('admin');
     }
   }
 });

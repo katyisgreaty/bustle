0c4b354ff5b5f02654fcdd77b1d3a6ca87255b8a
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-03-21 16:00:20 -0700

    remove red background color

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -28,10 +28,10 @@ export default Ember.Route.extend({
 
     update(article, params) {
       Object.keys(params).forEach(function(key) {
-   if(params[key]!==undefined) {
-     article.set(key,params[key]);
-   }
- });
+        if(params[key]!==undefined) {
+          article.set(key,params[key]);
+        }
+      });
       article.save();
       this.transitionTo('index');
     }
